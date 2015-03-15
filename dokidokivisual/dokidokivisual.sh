b9f28229e62298e6b1ae0b880260a1cb4b00a93b
tyage <namatyage@gmail.com>
2015-03-15 23:00:05 +0900

    added dokidokivisual bot

---
+++ dokidokivisual/dokidokivisual.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+source ../slack-config.sh
+
+CHANNEL=dokidokivisual
+declare -A MAGAZINES=([9]='まんがタイムきらら' [15]='まんがタイムきららミラク' [19]='まんがタイムきららMAX' [24]='まんがタイムきららフォワード' [28]='まんがタイムキャラット')
+
+magazine=${MAGAZINES[$(date +%d)]}
+uri="https://$SLACK_TEAM.slack.com/services/hooks/slackbot?token=$SLACK_TOKEN&channel=%23$CHANNEL"
+if [ -n "$magazine" ]; then
+    curl --data "今日は$magazineの発売日です。忘れずに買いにいきましょう。" $uri
+fi
