9e85d44778bce7ffcb2b2865452f8a9438f79a1e
tyage <namatyage@gmail.com>
2015-03-15 23:00:19 +0900

    fixed a bug

--- garbage/garbage.sh
+++ garbage/garbage.sh
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 source ../slack-config.sh
+
 CHANNEL=active
 declare -a GARBAGES=('' ':fire:一般ごみ' ':recycle:缶・ビン・ペットボトル' '' ':fire:一般ごみ' '' '');
 
 garbage=${GARBAGES[$(date +%w)]}
 uri="https://$SLACK_TEAM.slack.com/services/hooks/slackbot?token=$SLACK_TOKEN&channel=%23$CHANNEL"
 if [ -n "$garbage" ]; then
-    curl --data "明日は$garbage回収の日です。忘れずに出しておきましょう。" uri
+    curl --data "明日は$garbage回収の日です。忘れずに出しておきましょう。" $uri
 fi
